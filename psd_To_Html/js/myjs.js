9c59afa13b511c590ffaee90202d9244bc38bc9e
zihad07 <zihadnayem8gmail.com>
2019-09-08 09:01:10 +0600

    Work with Our Work, Our Team and Client Testimonials Sections

--- psd_To_Html/js/myjs.js
+++ psd_To_Html/js/myjs.js
@@ -28,6 +28,79 @@ $(document).ready(function(){
        
     });
 
+
+    $('.portfolio-carousel').carouFredSel({
+        responsive:true,
+		width:'100%',
+		circular:true,
+		prev:'#prev',
+		next:'#next',
+		scroll:{
+			items:1,
+			duration:500,
+			pauseOnHover: true
+		},
+		auto:true,
+		items:{
+			visible:{
+				min:1,
+				max:4
+			},
+		height:"variable"
+		}
+       
+    });
+
+
+
+    $('.team-carousel').carouFredSel({
+        responsive: true,
+        widht: '100%',
+        circular: true,
+        prev: '#team-prev',
+        next: '#team-next',
+
+        scroll: {
+            items:1,
+            duration:500,
+            pauseOnHover:true
+        },
+
+        auto: true,
+        items: {
+            visible: {
+                min:1,
+                max:3
+            },
+            height: "variable"
+        }
+
+    });
+
+    $('.testimonial-carousel').carouFredSel({
+
+		responsive:true,
+		width:'100%',
+		circular:true,
+		scroll:{
+			items:1,
+			duration:500,
+			pauseOnHover: true
+		},
+		auto:true,
+		items:{
+			visible:{
+				min:1,
+				max:1
+			},
+		height:"variable"
+		},
+		pagination:{
+			container:".testipager",
+			anchorBuilder: false
+		}
+	});
+
     $(window).scroll(function(){
         var top = $(this).scrollTop();
 
